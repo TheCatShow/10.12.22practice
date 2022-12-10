75d515acd391108163b2a84325f2a9bdc7946a2a
TheCatShow <75573235+TheCatShow@users.noreply.github.com>
2022-12-10 13:18:29 +0300

    Add files via upload

--- practice27/script.js
+++ practice27/script.js
@@ -1,9 +1,19 @@
-const container = document.querySelector('#container');
-const myButton = document.querySelector('#myButton');
+let container = document.createElement('div');
+container.textContent = 'Текст внутри контейнера';
+document.body.appendChild(container);
 
-element.textContent = 'Текст внутри контейнера';
-
+let myButton = document.createElement('button');
 myButton.textContent = 'Применить';
+document.body.appendChild(myButton);
+
+container.addEventListener('click', function(){
+    container.textContent = container.textContent.toUpperCase();
+    title.textContent = title.textContent.toUpperCase();
+})
+
+let text = document.createElement('div');
+text.textContent = '';
+document.body.appendChild(text);
 
 myButton.addEventListener('click', function(){
     text.textContent = 'Блок создан при клике на кнопку';
